39c15ca38802ecc531fbb53eca73a30d711a3181
user_2 <user_2@FACE-29>
2019-10-09 20:54:51 -0700

    test passed

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -1,6 +1,5 @@
 package main;
 import java.util.*; 
-
 public class Calculator {
     public static void main(String[] args)
     {
@@ -33,31 +32,25 @@ public class Calculator {
         }
 
 
-
     }
-    public static int add(int x, int y)
+    public static  int add(int x, int y)
     {
-    	int result=0;
-//        write your code here 
-    	
+    	int result=x+y;
         return result;
     }
     public static int sub(int x, int y)
     {
-    	int result=0;
-//        write your code here
+    	int result=x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
-    	int result=0;
-//      write your code here        
+    	int result=x*y;      
     	return result;
     }
     public static int div(int x, int y)
     {
-    	int result=0;
-//      write your code here
+    	int result=x/y;
     	return result;
     }
 
